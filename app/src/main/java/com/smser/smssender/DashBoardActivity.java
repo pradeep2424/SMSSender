278b8553f92be42334ec3c0fbf5ceb2669326981
pjadhav <pradeepjadhav18@gmail.com>
2019-12-09 18:02:03 +0530

    test

--- app/src/main/java/com/smser/smssender/DashBoardActivity.java
+++ app/src/main/java/com/smser/smssender/DashBoardActivity.java
@@ -28,7 +28,6 @@ public class DashBoardActivity extends AppCompatActivity implements
         NavigationView.OnNavigationItemSelectedListener, Constants {
 
     private DrawerLayout drawer;
-
     private ProgressDialog progressDialog;
 
     @Override
